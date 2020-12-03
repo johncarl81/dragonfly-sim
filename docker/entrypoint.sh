3646390f57b3a0fb7fdf412a4921cb6ed0d6b25b
John Ericksen <johncarl81@gmail.com>
2020-12-03 14:47:40 -0700

    Added juav and iris models

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 source /opt/ros/melodic/setup.bash
 source /workspace/install/setup.bash
 
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:"/workspace/src/dragonfly_sim/models"
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:"/workspace/ardupilot_gazebo/build"
+
 exec "$@"
