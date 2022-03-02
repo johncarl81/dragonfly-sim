2c1aa6416b8b2b7d5de1cd1221cd69115b683b85
Carter Frost <git@carterfrost.com>
2022-03-02 10:06:47 -0700

    src/dragonfly_sim/launch/apm.launch.py: hardcoded dragonfly1 namespace
    @TODO revert me

--- run.sh
+++ run.sh
@@ -25,4 +25,4 @@ docker run -it \
     --ip 172.18.0.2 \
     -p 11311:11311 \
     --network ros-net \
-    dragonfly-sim:latest --drones 4 --location BALLOON_FIESTA
+    dragonfly-sim:latest --drones 1 --location BALLOON_FIESTA
