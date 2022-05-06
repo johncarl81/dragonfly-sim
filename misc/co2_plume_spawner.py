5d804df74033405b18c1c10b145522419e0d81e4
Carter Frost <git@carterfrost.com>
2022-05-06 13:34:34 -0600

    misc/co2_plume_spawner.py: completed ros1 & 2 compatible

--- misc/co2_plume_spawner.py
+++ misc/co2_plume_spawner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from stl import mesh
 import numpy as np
 import math
@@ -6,13 +7,30 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import random
 from skimage import measure
 from skimage.draw import ellipsoid
-# import rclpy #ros2
-import rospy  # ros1
+
+ros1 = True
+try:
+  import rospy  # ros1
+except ModuleNotFoundError:
+  ros1 = False
+  import rclpy  # ros2
+  from gazebo_msgs.srv import SpawnEntity
+
 from geometry_msgs.msg import Point, Pose
-# from gazebo_msgs.srv import SpawnEntity
 from gazebo_msgs.srv import SpawnModel
 
+"""
+To try it out try something like this
+docker exec -it `docker ps | grep dragonfly-sim | cut -d" " -f1` /bin/bash
+source /opt/ros/galactic/setup.bash
+cd workspace
+python3.8 ./misc/co2_plume_spawner.py
+"""
+
 THRESHOLD = 300  # ppm
+node = None
+spawn_model = None
+spawn_entity_client = None
 
 
 def plume(x, y, z, q=5000, k=2, u=1):
@@ -27,40 +45,40 @@ def plume_grid():
 plume_base = plume_grid()
 
 xml = open("./gazebo/src/dragonfly_sim/models/co2_shell/model.sdf", "r").read().replace("\n", "")
-# rclpy.init()
-# node = rclpy.create_node('co2_plume_spawner2')
-# spawn_entity_client = node.create_client(srv_type=SpawnEntity, srv_name='spawn_entity')
-# spawn_entity_client.wait_for_service(timeout_sec=1.0)
-# rclpy.spin_once(node)
 
-spawn_model = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-rospy.init_node("co2_plume_spawner2")
+if ros1:
+  spawn_model = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+  rospy.init_node("co2_plume_spawner")
+else:
+  rclpy.init()
+  node = rclpy.create_node('co2_plume_spawner')
+  spawn_entity_client = node.create_client(srv_type=SpawnEntity, srv_name='spawn_entity')
+  spawn_entity_client.wait_for_service(timeout_sec=1.0)
 
-for thres_div in range(1, 6):
+for threshold_div in range(1, 6):
   # Use marching cubes to obtain the surface mesh of these ellipsoids
-  vertices, faces, _, values = measure.marching_cubes(plume_base, THRESHOLD / thres_div)
+  vertices, faces, _, values = measure.marching_cubes(plume_base, THRESHOLD / threshold_div)
   cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
   for i, f in enumerate(faces):
     for j in range(3):
       cube.vectors[i][j] = vertices[f[j], :]
-  stl_filename = '/tmp/plume' + str(thres_div) + '.stl'
+  stl_filename = '/tmp/plume' + str(threshold_div) + '.stl'
   cube.save(stl_filename)
   plume_co2_avg = sum(values) / len(values)
 
   new_xml = xml.replace("<uri>model://co2_shell/cube.stl</uri>", "<uri>{}</uri>".format(stl_filename)).replace(
     "<ambient>0 0 0 1</ambient>", "<ambient>{red} {green} {blue} {trans}</ambient>".format(
-      red=(6 - thres_div) / 6.0,
+      red=(6 - threshold_div) / 6.0,
       green=0,
-      blue=thres_div / 6.0,
+      blue=threshold_div / 6.0,
       trans=0.5))
-  spawn_model("plume" + str(thres_div), new_xml, "", Pose(), "world")
-  '''
-    req = SpawnEntity.Request()
-    req.xml = xml.replace("<uri>model://co2_shell/cube.stl</uri>", "<uri>model://{}</uri>".format(stl_filename))
-    req.xml = req.xml.replace("<diffuse>0 1 0 0</diffuse>", "<diffuse>{red} {green} {blue} 0</diffuse>".format(
-        red=255 / plume_co2_avg,
-        green=plume_co2_avg, blue=0))
+  if ros1:
+    spawn_model("plume" + str(threshold_div), new_xml, "", Pose(), "world")
+  else:  # ros2
+    req = SpawnEntity.Request(xml=new_xml)
+    req.name = "plume" + str(threshold_div)
     req.initial_pose.position = Point(x=0.0, y=0.0, z=0.0)  # shell_offset
-    #spawn_entity_client.call_async(req)
-    '''
+    spawn_entity_client.call_async(req)
+if not ros1:
+  rclpy.spin_once(node)
 print("Done")
